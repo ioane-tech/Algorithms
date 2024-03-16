329b60ee0b4418e6f38d625bd391eef98476a3d9
ioane-tech <ioturman4@gmail.com>
2024-03-16 15:30:37 +0400

    a

--- A level algorithms/tictactoe.js
+++ A level algorithms/tictactoe.js
@@ -1,15 +1,15 @@
 var tictactoe = function(moves) {
     const winConditions = [[[0,0],[0,1],[0,2]],[[1,0],[1,1],[1,2]],[[2,0],[2,1],[2,2]],[[0,0],    [1, 0],[2,0]],[[0,1],[1,1],[2,1]],[[0,2],[1,2],[2,2]],[[0,0],[1,1],[2,2]],[[0,2],[1,1],[2,0]]];
        
-       let grid = [[0,0,0],[0,0,0],[0,0,0]]; // Empty grid
+       let board = [[0,0,0],[0,0,0],[0,0,0]]; 
        
        for (let i = 0; i < moves.length; i++) {
            let [row, col] = moves[i];
-           grid[row][col] = i % 2 === 0 ? 1 : -1; // Assign 1 for player A and -1 for player B
+           board[row][col] = i % 2 === 0 ? 1 : -1; 
            for (const condition of winConditions) {
                let sum = 0;
                for (const [r, c] of condition) {
-                   sum += grid[r][c];
+                   sum += board[r][c];
                }
                if (Math.abs(sum) === 3) {
                    return sum === 3 ? 'A' : 'B';
