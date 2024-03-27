9904c1ac984c2ae2bfffab8d41d65976d33a3fc9
ioane-tech <ioturman4@gmail.com>
2024-03-27 18:58:35 +0400

    solved A level algorithm lemonade change

--- C level algorithms/multiplyString.js
+++ C level algorithms/multiplyString.js
@@ -7,8 +7,8 @@ var multiply = function(num1, num2) {
     numFirst = num1
     numSecond = num2
 
-    answer = `${numFirst * numSecond}`
+    answer = `${BigInt(numFirst * numSecond)}`
     console.log(typeof answer)
     return answer
 };
-console.log(multiply("2","3"))
+console.log(multiply("2903284902384092384092384092384902384092348","339024720947029374092374092374092374902374092374092374"))
